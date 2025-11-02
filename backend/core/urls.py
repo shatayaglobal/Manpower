1a3492f60f5a481ed1033d757fce827e29bae1a0
brenda-collab <shatayaglobal@gmail.com>
2025-11-02 14:49:00 +0300

    updates

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from django.conf.urls.static import static
 from django.conf import settings
+import os
 
 
 urlpatterns = [
