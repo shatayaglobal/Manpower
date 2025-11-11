a423b3e8cad4e1e3904c87f4ef5393e75c258bd8
brenda-collab <shatayaglobal@gmail.com>
2025-11-12 00:57:46 +0300

    updates

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -154,15 +154,6 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_FROM = env('EMAIL_FROM')
-DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
-
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
